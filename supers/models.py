98ae86a884b8e0997a12716659b5ad7953c4c377
bdavis83 <bdavis83@gmail.com>
2023-01-10 12:56:01 -0500

    Added supers to the apps on settings.py + made migration to push supers to the database + created first super

--- supers/models.py
+++ supers/models.py
@@ -7,5 +7,5 @@ class Supers(models.Model):
     alter_ego = models.CharField(max_length=255)
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
-    catch_phrase = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE, null=True)
